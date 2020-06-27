968ef4e6dbc1f1d0baa61917956c4d65058b034c
Apstoddard <andrewpstoddard@gmail.com>
2020-06-27 16:10:20 -0700

    hello

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Carson with a y is better</Text>
+      <Text>Carson with a n is better</Text>
     </View>
   );
 }
