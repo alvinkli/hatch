c2e21559ad7b55c210ecf34700ab33e97d0fd969
Apstoddard <andrewpstoddard@gmail.com>
2020-07-13 12:08:58 -0700

    Icons

--- pages/Home.js
+++ pages/Home.js
@@ -2,6 +2,7 @@ import React from "react";
 import { View, Text, Button } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Ionicons } from "@expo/vector-icons";
 
 import RestaurantsScreen from "./Restaurants/Restaurants";
 import OrdersScreen from "./Orders/Orders";
@@ -11,7 +12,21 @@ const Tab = createBottomTabNavigator();
 
 const HomeScreen = (props) => {
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+          if (route.name === "Restaurants") {
+            iconName = "ios-restaurant";
+          } else if (route.name === "Orders") {
+            iconName = "ios-list";
+          } else if (route.name === "Account") {
+            iconName = "ios-person";
+          }
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+      })}
+    >
       <Tab.Screen name="Restaurants" component={RestaurantsScreen} />
       <Tab.Screen name="Orders" component={OrdersScreen} />
       <Tab.Screen name="Account" component={AccountScreen} />
