c80db781cc50ab042b504239ead1fe9476d90ddb
Apstoddard <andrewpstoddard@gmail.com>
2020-07-13 13:44:51 -0700

    Account Nav

--- navigation/AccountNav.js
+++ navigation/AccountNav.js
@@ -1,8 +1,48 @@
 import React from "react";
-import { View, Text, Button } from "react-native";
+import { createStackNavigator } from "@react-navigation/stack";
 
-const AccountNav = (props) => {
-  return <Text>Account Screen</Text>;
-};
+import AccountDetailsScreen from "../screens/Account/AccountDetailsScreen";
+import AccountOptionsScreen from "../screens/Account/AccountOptionsScreen";
+import HelpScreen from "../screens/Account/HelpScreen";
+import ManageReviewsScreen from "../screens/Account/ManageReviewsScreen";
+import NotificationsScreen from "../screens/Account/NotificationsScreen";
+import PaymentInfoScreen from "../screens/Account/PaymentInfoScreen";
 
-export default AccountNav;
+const Stack = createStackNavigator();
+
+export default function OrdersNav() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="AccountDetailsScreen"
+        component={AccountDetailsScreen}
+        options={{ title: "Account" }}
+      />
+      <Stack.Screen
+        name="AccountOptionsScreen"
+        component={AccountOptionsScreen}
+        options={{ title: "Accoungt Options" }}
+      />
+      <Stack.Screen
+        name="HelpScreen"
+        component={HelpScreen}
+        options={{ title: "Help" }}
+      />
+      <Stack.Screen
+        name="ManageReviewsScreen"
+        component={ManageReviewsScreen}
+        options={{ title: "Manage Reviews" }}
+      />
+      <Stack.Screen
+        name="NotificationsScreen"
+        component={NotificationsScreen}
+        options={{ title: "Notifications" }}
+      />
+      <Stack.Screen
+        name="PaymentInfoScreen"
+        component={PaymentInfoScreen}
+        options={{ title: "Payment Info" }}
+      />
+    </Stack.Navigator>
+  );
+}
--- navigation/OrdersNav.js
+++ navigation/OrdersNav.js
@@ -1,8 +1,27 @@
 import React from "react";
-import { View, Text, Button } from "react-native";
+import { createStackNavigator } from "@react-navigation/stack";
 
-const OrdersNav = (props) => {
-  return <Text>Orders Screen</Text>;
-};
+const Stack = createStackNavigator();
 
-export default OrdersNav;
+export default function OrdersNav() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="UserAuthScreen"
+        component={UserAuthScreen}
+        options={{ title: "Hatch" }}
+      />
+      <Stack.Screen
+        name="SignUpScreen"
+        component={SignUpScreen}
+        options={{ title: "Sign Up", headerBackTitle: "Back" }}
+      />
+      <Stack.Screen
+        name="LoginScreen"
+        component={LoginScreen}
+        options={{ title: "Login", headerBackTitle: "Back" }}
+      />
+      <Stack.Screen name="MainNav" component={MainNav} />
+    </Stack.Navigator>
+  );
+}
