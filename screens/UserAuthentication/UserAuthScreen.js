92b615bf854c98f3a9cde3f16004857b6f695473
Apstoddard <andrewpstoddard@gmail.com>
2020-07-16 22:02:36 -0700

    Stuff

--- screens/UserAuthentication/UserAuthScreen.js
+++ screens/UserAuthentication/UserAuthScreen.js
@@ -1,40 +1,51 @@
 import React from "react";
-import { View, Text, Button } from "react-native";
+import { View, Image } from "react-native";
 import LargeButton from "../../components/LargeButton";
 import AppText from "../../components/AppText";
 import Colors from "../../constants/Colors";
+import Logo from "../../components/Logo";
 
 const UserAuthScreen = (props) => {
   return (
     <View
-      style={{ flexDirection: "column", flex: 1, justifyContent: "center" }}
+      style={{
+        flexDirection: "column",
+        height: 300,
+        flex: 1,
+        justifyContent: "center",
+      }}
     >
-      <View
-        style={{
-          flexDirection: "column",
-          flex: 1,
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <AppText
-          text="HATCH"
-          bold={true}
-          style={{ fontSize: 48, color: Colors.primary }}
-        />
-      </View>
       <View
         style={{ flexDirection: "column", flex: 1, justifyContent: "center" }}
       >
-        <LargeButton
-          text="Sign Up"
-          onPress={() => props.navigation.navigate("SignUpScreen")}
-        />
-        <LargeButton
-          text="Login"
-          accent={true}
-          onPress={() => props.navigation.navigate("LoginScreen")}
-        />
+        <View
+          style={{
+            flexDirection: "column",
+            flex: 1,
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Logo />
+          <AppText
+            text="HATCH"
+            bold={true}
+            style={{ fontSize: 48, color: Colors.primary }}
+          />
+        </View>
+        <View
+          style={{ flexDirection: "column", flex: 1, justifyContent: "center" }}
+        >
+          <LargeButton
+            text="Sign Up"
+            onPress={() => props.navigation.navigate("SignUpScreen")}
+          />
+          <LargeButton
+            text="Login"
+            accent={true}
+            onPress={() => props.navigation.navigate("LoginScreen")}
+          />
+        </View>
       </View>
     </View>
   );
