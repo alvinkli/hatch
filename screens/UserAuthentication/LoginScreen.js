4410e340b86e5359855f4e47bb511b1a7c94455f
Alvin Li <alvinklj@gmail.com>
2020-07-21 21:44:24 -0700

    keyboard hides when clicking outside

--- screens/UserAuthentication/LoginScreen.js
+++ screens/UserAuthentication/LoginScreen.js
@@ -1,5 +1,10 @@
 import React from "react";
-import { View, SafeAreaView, KeyboardAvoidingView } from "react-native";
+import {
+  View,
+  SafeAreaView,
+  ScrollView,
+  KeyboardAvoidingView,
+} from "react-native";
 import Input from "../../components/Input";
 import LargeButton from "../../components/LargeButton";
 import Logo from "../../components/Logo";
@@ -9,42 +14,44 @@ import Colors from "../../constants/Colors";
 const LoginScreen = (props) => {
   const { login } = props.route.params;
   return (
-    <SafeAreaView>
-      <KeyboardAvoidingView behavior="position">
-        <View
-          style={{
-            alignItems: "center",
-            paddingTop: 32,
-          }}
-        >
-          <Logo />
-          <AppText
-            text="HATCH"
-            bold={true}
-            style={{ fontSize: 48, color: Colors.primary }}
-          />
-        </View>
-        <View style={{ paddingTop: 32 }}>
-          <AppText
-            text="Enter Account Info"
-            medium={true}
-            style={{ marginLeft: "9%" }}
-          />
-          <Input
-            placeholder="Email"
-            keyboardType="email-address"
-            autoCapitalize="none"
-          />
-          <Input placeholder="Password" secureTextEntry={true} />
-          <LargeButton
-            text="Login"
-            accent={true}
-            onPress={() => props.navigation.navigate("LoginScreen")}
-          />
-        </View>
-      </KeyboardAvoidingView>
-      <View style={{ padding: 20 }} />
-    </SafeAreaView>
+    <ScrollView scrollEnabled={false}>
+      <SafeAreaView>
+        <KeyboardAvoidingView behavior="position">
+          <View
+            style={{
+              alignItems: "center",
+              paddingTop: 32,
+            }}
+          >
+            <Logo />
+            <AppText
+              text="HATCH"
+              bold={true}
+              style={{ fontSize: 48, color: Colors.primary }}
+            />
+          </View>
+          <View style={{ paddingTop: 32 }}>
+            <AppText
+              text="Enter Account Info"
+              medium={true}
+              style={{ marginLeft: "9%" }}
+            />
+            <Input
+              placeholder="Email"
+              keyboardType="email-address"
+              autoCapitalize="none"
+            />
+            <Input placeholder="Password" secureTextEntry={true} />
+            <LargeButton
+              text="Login"
+              accent={true}
+              onPress={() => props.navigation.navigate("LoginScreen")}
+            />
+          </View>
+        </KeyboardAvoidingView>
+        <View style={{ padding: 20 }} />
+      </SafeAreaView>
+    </ScrollView>
   );
 };
 
--- screens/UserAuthentication/SignUpScreen.js
+++ screens/UserAuthentication/SignUpScreen.js
@@ -1,5 +1,10 @@
 import React from "react";
-import { View, SafeAreaView, KeyboardAvoidingView } from "react-native";
+import {
+  View,
+  SafeAreaView,
+  KeyboardAvoidingView,
+  ScrollView,
+} from "react-native";
 import Input from "../../components/Input";
 import LargeButton from "../../components/LargeButton";
 import Logo from "../../components/Logo";
@@ -9,51 +14,53 @@ import Colors from "../../constants/Colors";
 const SignUpScreen = (props) => {
   const { login } = props.route.params;
   return (
-    <SafeAreaView>
-      <KeyboardAvoidingView behavior="position">
-        <View
-          style={{
-            alignItems: "center",
-            paddingTop: 32,
-          }}
-        >
-          <Logo />
-          <AppText
-            text="HATCH"
-            bold={true}
-            style={{ fontSize: 48, color: Colors.primary }}
-          />
-        </View>
-        <View style={{ paddingTop: 32 }}>
-          <AppText
-            text="Create An Account"
-            medium={true}
-            style={{ marginLeft: "9%" }}
-          />
-          <Input placeholder="First Name" />
-          <Input placeholder="Last Name" />
-          <Input
-            placeholder="Email"
-            keyboardType="email-address"
-            autoCapitalize="none"
-          />
-          <Input placeholder="Password" secureTextEntry={true} />
-          <LargeButton
-            text="Sign Up"
-            onPress={() => props.navigation.navigate("LoginScreen")}
-          />
-          <AppText
+    <ScrollView scrollEnabled={false}>
+      <SafeAreaView>
+        <KeyboardAvoidingView behavior="position">
+          <View
             style={{
-              marginTop: 12,
-              marginLeft: "9%",
-              color: Colors.secondary,
+              alignItems: "center",
+              paddingTop: 32,
             }}
-            text="Already have an account? Sign in here"
-          />
-        </View>
-        <View style={{ height: 20 }} />
-      </KeyboardAvoidingView>
-    </SafeAreaView>
+          >
+            <Logo />
+            <AppText
+              text="HATCH"
+              bold={true}
+              style={{ fontSize: 48, color: Colors.primary }}
+            />
+          </View>
+          <View style={{ paddingTop: 32 }}>
+            <AppText
+              text="Create An Account"
+              medium={true}
+              style={{ marginLeft: "9%" }}
+            />
+            <Input placeholder="First Name" />
+            <Input placeholder="Last Name" />
+            <Input
+              placeholder="Email"
+              keyboardType="email-address"
+              autoCapitalize="none"
+            />
+            <Input placeholder="Password" secureTextEntry={true} />
+            <LargeButton
+              text="Sign Up"
+              onPress={() => props.navigation.navigate("LoginScreen")}
+            />
+            <AppText
+              style={{
+                marginTop: 12,
+                marginLeft: "9%",
+                color: Colors.secondary,
+              }}
+              text="Already have an account? Sign in here"
+            />
+          </View>
+          <View style={{ height: 20 }} />
+        </KeyboardAvoidingView>
+      </SafeAreaView>
+    </ScrollView>
   );
 };
 
